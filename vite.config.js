1d03dacb0ee334ccdedb951fa65a43e9b3987a60
Sdmrf <syedmarufali23@gmail.com>
2024-03-07 20:59:14 +0530

    refactor : change base in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-template/",
+  base: "/React-Template/",
   plugins: [react()],
 });
