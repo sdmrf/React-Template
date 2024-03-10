1be2cd9e5b41e3f2cb968fc6c3d230c12055205d
Sdmrf <syedmarufali23@gmail.com>
2024-03-10 21:29:36 +0530

    fix : minor bug fixes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  // Add Your Github Repository Name Here
   base: "/React-Template/",
   plugins: [react()],
 });
